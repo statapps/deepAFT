5e9bc7593a2a53b9b1d830f41085ecf18ca700b0
Bingshu E. Chen <bingshu.chen@queensu.ca>
2019-06-11 11:08:42 -0400

    add transform method

--- R/deepAFT.R
+++ R/deepAFT.R
@@ -29,7 +29,8 @@ deepAFT.formula = function(formula, model, data, control = list(...),
   #fit = do.call("deepAFT", x, y, model, control)
 
   fit = switch(method, BuckleyJames=deepAFT.default(x, y, model, control), 
-                       ipcw = deepAFT.ipcw(x, y, model, control))
+                       ipcw = deepAFT.ipcw(x, y, model, control),
+                       transform = deepAFT.trans(x, y, model, control))
   return(fit)
 }
 
@@ -109,11 +110,11 @@ deepAFT.ipcw = function(x, y, model, control, ...){
   #n = length(status)
   #max.t = max(time)
   
-  # fit km curve for censoring
+  # fit km curve for censoring, set surv to small number if last obs fails.
   G_fit = survfit(Surv(time, 1-status)~1)
-  G = status/(.appxf(G_fit$surv, x=G_fit$time, xout = time) + 1e-5)
+  G_fit$surv = ifelse(G_fit$surv > 0, G_fit$surv, 1e-5)
+  G = status/.appxf(G_fit$surv, x=G_fit$time, xout = time)
   
-  #print(G)
   lgt = log(time)
   mean.ipt = mean(lgt)
   lgt = lgt-mean.ipt
@@ -133,6 +134,56 @@ deepAFT.ipcw = function(x, y, model, control, ...){
   return(object)
 }
 
+deepAFT.trans = function(x, y, model, control, ...){
+  ## epochs = 30, batch_size = 32, validation_split = 0.1, verbose = 1) {
+  epochs = control$epochs
+  batch.n = control$batch.n
+  v_split = control$v_split
+  verbose = control$verbose
+  
+  time = y[, 1]
+  status = y[, 2]
+  #n = length(status)
+  #max.t = max(time)
+  
+  # fit km curve for censoring, set surv to small number if last obs fails.
+  # transformation based on book of Fan J (1996, page 168)
+  Gfit = survfit(Surv(time, 1-status)~1)
+  St = Gfit$surv
+  tm = Gfit$time
+  St = ifelse(St> 0, St, 1e-5)
+  Gt = 1/.appxf(St, x=tm, xout = time)
+  
+  # Integrate from 0 to t of 1/G(u)
+  dt = diff(c(0, tm))
+  iG = cumsum(1/St*dt)
+  iGt = .appxf(iG, x=tm, xout = time)
+
+  a = min(((iGt - time)/(time*Gt-iGt))[status==1], na.rm=TRUE)
+  phi2 = (1+a)*iGt
+  phi1 = phi2 - a*time*Gt
+  
+  time2 = ifelse(status>0, phi1, phi2)
+  
+  lgt = log(time2)
+  mean.ipt = mean(lgt)
+  lgt = lgt-mean.ipt
+  
+  history = model%>%fit(x, lgt,
+                        epochs = epochs, batch_size = batch.n, sample_weight = G,
+                        validation_split = v_split, verbose = verbose)
+  
+  #linear predictors
+  lp = (model%>%predict(x)+mean.ipt)
+  pred_time = exp(lp)
+  ### create outputs
+  object = list(x = x, y = y, model = model, mean.ipt = mean.ipt, 
+                mu = lp, means = apply(x, 2, mean),
+                risk = exp(-lp), method = "transform")
+  class(object) = 'deepAFT'
+  return(object)
+}
+
 deepAFTcontrol = function(epochs = 30, batch.n = 32,
   v_split = 0.1, verbose = 0, epsilon = 0.01, max.iter = 50) {
   
@@ -144,7 +195,7 @@ plot.deepAFT = function(x, type = c('predicted', 'residuals', 'baselineKM'), ...
   time = x$y[, 1]
   log.time  = log(time)
   if (type == 'predicted') {
-    predicted = x$linear.predictors
+    predicted = x$mu
     plot(log.time, predicted, xlab = 'Log survival time', ylab = 'Predicted log survival time')
     abline(0, 1, lty = 2)
   } else if(type == 'residuals') {
@@ -158,8 +209,21 @@ plot.deepAFT = function(x, type = c('predicted', 'residuals', 'baselineKM'), ...
 }
 
 print.deepAFT = function(x, ...) {
+  cat("Deep AFT model with ", x$method, 'method\n\n')
+  risk = as.vector(x$risk)
+  y = x$y
+  location = (1/as.vector(risk))
+  cat("Summary of predict location score exp(mu):\n")
+  print(summary(location))
+  
+  cat("\n\nDistribution of T0 = T/exp(mu):\n")
+  
   sfit = survfit(x)
   print(sfit)
+  
+  cindex = survConcordance(y~risk)
+  cat("Concordance index: ", cindex$concordance, "\n")
+  #print(cindex)
 }
 
 #### impute KM for AFT
